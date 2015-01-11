ac82b800f4868964ed62ca018eb5eff15506ae30
Selim Onat <copter@ganglion.fritz.box>
2015-01-11 22:52:37 +0100

    better commenting…

--- random_street_view.py
+++ random_street_view.py
@@ -1,3 +1,9 @@
+#Example usage: python random_street_view.py DEU
+# it will generate a random coordinate based on the country mask, will then transform this coordinate
+# to human readable address (e.g. street, number etc.) by asking google, and finally will capture a street
+# view from that point. It is possible that that there is no street view at that location, in that case, it will 
+# reiterate until success.
+
 import argparse
 import os
 import random
@@ -17,15 +23,14 @@ except:
 # See https://developers.google.com/maps/documentation/streetview/
 API_KEY = open('/Users/copter/Documents/googleapi/apikey.txt','r').read()
 GOOGLE_URL = "http://maps.googleapis.com/maps/api/streetview?sensor=false&size=640x640&fov=120&key=" + API_KEY
-# LL will be replaced by the candidate coordinate
+# LL will be replaced by the candidate coordinate (LL will be replaced accordingly)
 GOOGLE_DIR = "http://maps.googleapis.com/maps/api/directions/json?origin=LL&destination=LL"
 
 
 IMG_PREFIX = "img_"
 IMG_SUFFIX = ".jpg"
 
-parser = argparse.ArgumentParser(
-	description="Get random Street View images from a given country")
+parser = argparse.ArgumentParser(description="Get random Street View images from a given country")
 parser.add_argument('country',  help='ISO 3166-1 Alpha-3 Country Code')
 args = parser.parse_args()
 
@@ -59,6 +64,10 @@ if not os.path.exists(shape_file):
 sf = shapefile.Reader(shape_file)
 shapes = sf.shapes()
 
+#TODO:
+#here find the coordinates of the city of interest 
+
+
 print "Finding country"
 for i, record in enumerate(sf.records()):
 	if record[2] == args.country.upper():
@@ -74,7 +83,7 @@ for i, record in enumerate(sf.records()):
 print "Getting images"
 attempts, country_hits, imagery_hits, imagery_misses = 0, 0, 0, 0
 MAX_URLS = 25000
-IMAGES_WANTED = 10
+IMAGES_WANTED = 1
 
 if not os.path.exists(args.country):
 	os.makedirs(args.country)
@@ -82,6 +91,10 @@ if not os.path.exists(args.country):
 try:
 	while(True):
 		attempts += 1
+		#TODO:
+		#Here, get a random coordinate from a circle of a given diameter around the center.
+
+
 		rand_lat = random.uniform(min_lat, max_lat)
 		rand_lon = random.uniform(min_lon, max_lon)
 		# print attempts, rand_lat, rand_lon
@@ -106,7 +119,7 @@ try:
 				# Note: hardcoded based on current size of default.
 				# Might change.
 				# Will definitely change if you change requested image size.
-				if os.path.getsize(outfile) == 8381:  # bytes
+				if os.path.getsize(outfile) < 10000:  # bytes
 					print "    No imagery"
 					imagery_misses += 1
 					os.remove(outfile)
